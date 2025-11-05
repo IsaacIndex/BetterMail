15ddb4d7faf78ea2a4eb1fae4d4a0b672dde3adc
Isaac Wong <isaacwongnh@ibm.com>
2025-11-05 12:25:19 +0800

    Initial Commit

---
+++ BetterMail/BetterMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterMailApp.swift
+//  BetterMail
+//
+//  Created by Isaac IBM on 5/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
